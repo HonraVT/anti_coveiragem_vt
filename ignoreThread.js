c349a2d1aff9f86f52b8f4de8e7e09353f44d8ff
HonraVT <111161607+HonraVT@users.noreply.github.com>
2023-05-26 11:24:00 -0300

    Add files via upload

---
+++ ignoreThread.js
@@ -0,0 +1,34 @@
+chrome.storage.sync.get("data", () => {
+  muteCoveiragem();
+});
+
+function getThreads() {
+  return document.querySelectorAll(".js-threadList>.structItem");
+}
+
+function getThreadCreationDate(thread) {
+  try {
+    const stringDate = thread.querySelector(".structItem-startDate").innerText;
+    if (Date.parse(stringDate) == NaN) {
+      throw new Error("Invalid date");
+    }
+
+    return new Date(stringDate);
+  } catch (e) {
+    return null;
+  }
+}
+
+function muteCoveiragem() {
+  const threads = getThreads();
+  threads.forEach((thread) => {
+    const createdAt = getThreadCreationDate(thread);
+    if (createdAt == null) {
+      return;
+    }
+
+    if (createdAt < new Date("2022/11/11")) {
+      thread.style = "display: none;";
+    }
+  });
+}
