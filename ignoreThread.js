5abb5d26b51a9ad1e1c5f84b8afbc17fca31a73a
HonraVT <111161607+HonraVT@users.noreply.github.com>
2023-05-26 19:54:25 -0300

    Update ignoreThread.js

--- ignoreThread.js
+++ ignoreThread.js
@@ -1,7 +1,3 @@
-chrome.storage.sync.get("data", () => {
-  muteCoveiragem();
-});
-
 function getThreads() {
   return document.querySelectorAll(".js-threadList>.structItem");
 }
@@ -19,7 +15,7 @@ function getThreadCreationDate(thread) {
   }
 }
 
-function muteCoveiragem() {
+(() => {
   const threads = getThreads();
   threads.forEach((thread) => {
     const createdAt = getThreadCreationDate(thread);
@@ -31,4 +27,4 @@ function muteCoveiragem() {
       thread.style = "display: none;";
     }
   });
-}
+})();
