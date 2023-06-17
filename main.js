b2aefb242db2321c807b06c6d00d9baf0536d880
HonraVT <111161607+HonraVT@users.noreply.github.com>
2023-06-17 07:22:55 -0300

    Update and rename ignoreThread.js to main.js

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
-chrome.storage.sync.get("data", () => {
-  muteCoveiragem();
-});
+muteCoveiragem();
+hideImage();
 
 function getThreads() {
   return document.querySelectorAll(".js-threadList>.structItem");
 }
-
 function getThreadCreationDate(thread) {
   try {
     const stringDate = thread.querySelector(".structItem-startDate").innerText;
     if (Date.parse(stringDate) == NaN) {
       throw new Error("Invalid date");
     }
-
     return new Date(stringDate);
   } catch (e) {
     return null;
@@ -26,9 +23,22 @@ function muteCoveiragem() {
     if (createdAt == null) {
       return;
     }
-
     if (createdAt < new Date("2022/11/11")) {
       thread.style = "display: none;";
     }
   });
+}
+
+
+function hideImage() {
+const arr = [
+  'https://assets-prd.ignimgs.com/',
+  'https://oystatic.ignimgs.com/',
+  'https://oyster.ignimgs.com/'
+];
+const images = document.querySelectorAll('.avatar img');
+images.forEach((image) => {
+if (arr.every(url => !image.src.startsWith(url))) {
+image.style.display = 'none';}
+});
 }
